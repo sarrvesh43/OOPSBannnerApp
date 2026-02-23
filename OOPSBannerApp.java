986e699811b710fd8ac6b900c4d64db63a63f3e8
sarrvesh43 <sn9542@srmist.edu.in>
2026-02-23 10:22:05 +0530

    UC2 - Print Banner using class and object

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,28 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("=== OOPS Banner - ASCII Art ===");
+        System.out.println();
+        
+        // Each line manually constructed with string concatenation
+        String line1 = " **   **   **   **     *** ";
+        String line2 = "*    * *    * *    * *      ";
+        String line3 = "*    * *    * *    * *      ";
+        String line4 = "*    * *    * *    * **   ";
+        String line5 = "*    * *    * *    *     *  ";
+        String line6 = "*    * *    * *    *     *  ";
+        String line7 = " **   **   **  **    *** ";
+        
+        // Print each line sequentially
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+        
+        System.out.println();
+        System.out.println("=================================");
     }
 }
