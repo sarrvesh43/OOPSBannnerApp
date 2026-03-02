e3e70aa9f2c571862ff33147dd133048c557a35e
sarrvesh43 <sn9542@srmist.edu.in>
2026-03-02 08:37:35 +0530

    UC3 - Refactored banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,32 @@
+class Banner {
+
+    void displayBanner() {
+
+        String border = String.join("", "*","*","*","*","*","*","*","*","*","*",
+                                         "*","*","*","*","*","*","*","*","*","*","*","*","*","*");
+
+        String emptyLine = String.join("", "*",
+                "                      ",
+                "*");
+
+        String textLine = String.join("", "*",
+                "        OOPS          ",
+                "*");
+
+        System.out.println(border);
+        System.out.println(emptyLine);
+        System.out.println(textLine);
+        System.out.println(emptyLine);
+        System.out.println(border);
+    }
+}
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        Banner banner = new Banner();
+        banner.displayBanner();
+
     }
 }
