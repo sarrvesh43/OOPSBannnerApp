4fe63fd361b6e170934161417564019cfcbb1ec1
sarrvesh43 <sn9542@srmist.edu.in>
2026-03-24 21:51:46 +0530

    UC6 - Refactored using static methods for O, P, S (DRY principle)

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,32 +1,54 @@
 class Banner {
 
-    void displayBanner() {
-
-        String border = String.join("", 
-                "*","*","*","*","*","*","*","*","*","*",
-                "*","*","*","*","*","*","*","*","*","*",
-                "*","*","*","*");
-
-        String emptyLine = String.join("", "*",
-                "                      ",
-                "*");
-
-        String textLine = String.join("", "*",
-                "        OOPS          ",
-                "*");
-
-        // UC4: Store all lines inside an array
-        String[] bannerLines = {
-                border,
-                emptyLine,
-                textLine,
-                emptyLine,
-                border
+    // Static method for O
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
         };
+    }
 
-        // UC4: Loop through array and print
-        for (String line : bannerLines) {
-            System.out.println(line);
+    // Static method for P
+    static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static method for S
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+
+    // Display full OOPS banner
+    static void displayBanner() {
+
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        // Loop through rows
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
         }
     }
 }
@@ -34,9 +56,6 @@ class Banner {
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        Banner banner = new Banner();
-        banner.displayBanner();
-
+        Banner.displayBanner();
     }
 }
